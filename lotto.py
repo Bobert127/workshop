1619c821911913c5dabe421a889475f7fc9b3b90
Robert <roberttrzaska@outlook.com>
2021-06-05 11:31:27 +0200

    first code

--- lotto.py
+++ lotto.py
@@ -3,12 +3,20 @@ def check_number():
 
     while True:
         try:
-            input_number = input("Podaj liczbę z zakresu 1-49: ")
+            input_number = int(input("Podaj liczbę z zakresu 1-49: "))
             break
         except ValueError:
             print("It's not a number!")
     return input_number
 
 
+def check_numbers():
+    input_numbers = []
+    while len(input_numbers) < 6:
+        number = check_number()
+        if number not in input_numbers and 0 < number <== 49:
+            input_numbers.append(number)
+
+    return input_numbers
 
 
