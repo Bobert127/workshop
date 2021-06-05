0fbf8d7bb7d77d79b845731fa6381c2474d18caa
Robert <roberttrzaska@outlook.com>
2021-06-05 11:43:52 +0200

    loss number

--- lotto.py
+++ lotto.py
@@ -1,6 +1,8 @@
+import random
 from random import randint
-def check_number():
 
+
+def check_number():
     while True:
         try:
             input_number = int(input("Podaj liczbę z zakresu 1-49: "))
@@ -14,9 +16,18 @@ def check_numbers():
     input_numbers = []
     while len(input_numbers) < 6:
         number = check_number()
-        if number not in input_numbers and 0 < number <== 49:
+        if number not in input_numbers and 0 < number < 50:
             input_numbers.append(number)
-
     return input_numbers
 
+
+def list_numbers():
+    print(", ".join(str(input_numbers) for input_numbers in sorted(input_numbers)))
+
+def lotto_numbers():
+    numbers = list(range(1, 50))
+    random.shuffle(numbers)
+    return numbers[:6]
+
+def lotto():
 
