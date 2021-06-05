52bea7fd3784446ea2dee8600c5a64ac8818a8c5
Robert <roberttrzaska@outlook.com>
2021-06-05 12:19:53 +0200

    try

--- lotto.py
+++ lotto.py
@@ -16,7 +16,8 @@ def check_numbers():
     input_numbers = []
     while len(input_numbers) < 6:
         number = check_number()
-        if number not in input_numbers and 0 < number < 50:
+        #if number not in input_numbers and 0 < number < 50:
+        if number not in input_numbers and 0 < number <= 49:
             input_numbers.append(number)
     return input_numbers
 
@@ -32,12 +33,19 @@ def lotto_numbers():
 def lotto():
     mine_number = check_numbers()
     print("Your type: ")
-    print_numbers(mine_number)
+    list_numbers(mine_number)
 
     lotto_number = lotto_numbers()
     print("Lotto numbers: ")
-    print_numbers(lotto_number)
+    list_numbers(lotto_number)
 
     a = 0
-    for
+    for mine_number in mine_number:
+        if number in lotto_numbers:
+            a += 1
+    print(f"udało się")
+
+if __name__ == '__main__':
+
+    lotto()
 
