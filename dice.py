a2d4d99dfbc66b835c5229f3068aa107f415ef73
Robert <roberttrzaska@outlook.com>
2021-06-05 15:11:33 +0200

    start 2

--- dice.py
+++ dice.py
@@ -1,6 +1,16 @@
 import random
-def my_string('zDy+z'):
-    l = [my_string]
-    return l
-my_string()
+
+dice = ("D100", "D20", "D12", "D10", "D8", "D6", "D4", "D3")
+
+def my_funcjion(my_cod):
+    for e in dice:
+        if e in my_cod:
+            multi, mody = my_cod.split(e)
+        e_value -= int(e[1:])
+        break
+    else:
+        return "Wrong Input"
+#print(dice[1])
+
+
 
