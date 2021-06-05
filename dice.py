68bfb7a0fd6c4c214b245ed2f72bdd2957f4bae8
Robert <roberttrzaska@outlook.com>
2021-06-05 15:36:31 +0200

    start 4

--- dice.py
+++ dice.py
@@ -6,15 +6,15 @@ dice = ("D100", "D20", "D12", "D10", "D8", "D6", "D4", "D3")
 def my_funcjion(my_cod):
     for e in dice:
         if e in my_cod:
-            multi, modi = my_cod.split(e)
+            my, mr = my_cod.split(e)
         e_value = int(e[1:])
-        break
-    else:
-        return "Wrong Input"
-        multi = int(multi) if multi else 1
-        modi = int(modi) if modi else 0
+        #break
+    #else:
+        #return "Wrong Input"
+    my = int(my) if my else 1
+    mr = int(mr) if mr else 0
 
-    return sum([random.randint(1, e_value) for _ in range(multi)]) + modi
+    return sum([random.randint(1, e_value) for _ in range(my)]) + mr
 
 
 if __name__ == '__main__':
